d0f69d828693d27499704fc29c4231ec2f776edc
Jonathan <5377074j@gmail.com>
2021-12-02 15:08:03 +0200

    Solutions for day 2 of AOC2020

---
+++ 2020/day_2/code.py
@@ -0,0 +1,30 @@
+def read_file(path: str = "input") -> str:
+    with open(path) as file:
+        return file.read()
+
+
+def part_1():
+    ranges = [
+        (
+            [int(y) for y in x.split()[0].split('-')],
+            x.split()[1].strip(":"),
+            x.split()[2]
+        ) for x in read_file().splitlines()
+    ]
+    print("Solution for part 1 of day 1:", sum(r1 <= string.count(letter) <= r2 for (r1, r2), letter, string in ranges))
+
+
+def part_2():
+    ranges = [
+        (
+            [int(y) for y in x.split()[0].split('-')],
+            x.split()[1].strip(":"),
+            x.split()[2]
+        ) for x in read_file().splitlines()
+    ]
+
+    print("Solution for part 1 of day 1:", sum((string[r1-1] == letter) ^ (string[r2-1] == letter) for (r1, r2), letter, string in ranges))
+
+
+part_1()
+part_2()
